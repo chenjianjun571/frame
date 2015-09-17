efcb4b44545b1ef54f570a6599e1a59dc7af555e
CJJ <chenjianjun571@gmail.com>
2015-09-17 16:25:34 +0800

    luanyong

--- frame/src/network/PassiveTCPClient.cpp
+++ frame/src/network/PassiveTCPClient.cpp
@@ -12,15 +12,15 @@
 
 namespace NAME_SPACE {
     
-    void PassiveTCPTimeOutEventCb(evutil_socket_t fd, short, void *data) {
-        
+    void PassiveTCPTimeOutEventCb(evutil_socket_t fd, short, void *data)
+    {
         PassiveTCPClient *pPassiveTCPClient = (PassiveTCPClient*)data;
         // 心跳超时回调
         pPassiveTCPClient->ProcEvent(ENE_HEART);
     }
     
-    void PassiveTCPReadEventCb(struct bufferevent *bev, void *data) {
-
+    void PassiveTCPReadEventCb(struct bufferevent *bev, void *data)
+    {
         PassiveTCPClient* pPassiveTCPClient = (PassiveTCPClient*)data;
 
         static unsigned char recv_buf[RECV_DATA_MAX_PACKET_SIZE];
@@ -28,17 +28,18 @@ namespace NAME_SPACE {
         static PacketLength nbytes = 0;
 
         /** TCP网络通信的时候采用头两个字节为数据包长度的方式进行规范，防止粘包 */
-        do {
-
+        do
+        {
             nbytes = evbuffer_get_length(bev->input);
-            if (nbytes < kPacketLenSize) {
+            if (nbytes < kPacketLenSize)
+            {
                 return;
             }
 
             // 如果大于系统定义的最大包长度，为防止恶意行为需要做断开处理
             datalen = GetBE32(bev->input);
-            if (datalen > RECV_DATA_MAX_PACKET_SIZE) {
-
+            if (datalen > RECV_DATA_MAX_PACKET_SIZE)
+            {
                 LOG(INFO)<<"接收服务器的数据超过缓冲区大小,断开客户端.收到的数据长度:"<<datalen;
                 pPassiveTCPClient->ProcEvent(ENE_CLOSE);
 
@@ -46,7 +47,8 @@ namespace NAME_SPACE {
             }
 
             // 判断数据是否收集齐全，没有收集齐全的不做处理
-            if (nbytes < datalen) {
+            if (nbytes < datalen)
+            {
                 return;
             }
 
@@ -55,12 +57,11 @@ namespace NAME_SPACE {
 
             /// 数据接收回调,去除头四个字节的长度buf
             pPassiveTCPClient->PutRecvData(recv_buf+kPacketLenSize, datalen);
-
         } while (true);
-
     }
     
-    void PassiveTCPEventCb(struct bufferevent *bev, short events, void *data) {
+    void PassiveTCPEventCb(struct bufferevent *bev, short events, void *data)
+    {
         PassiveTCPClient* pPassiveTCPClient = (PassiveTCPClient*)data;
         // 处理事件
         pPassiveTCPClient->ProcEvent(ENE_CLOSE);
@@ -76,21 +77,24 @@ namespace NAME_SPACE {
     _pTCPClientSignal(nullptr)
     {}
     
-    PassiveTCPClient::~PassiveTCPClient() {
+    PassiveTCPClient::~PassiveTCPClient()
+    {
         StopWork();
         _pTCPClientSignal = nullptr;
     }
     
-    bool PassiveTCPClient::StartWork(TCPClientSignal* pTCPClientSignal) {
-        
-        if (_bev) {
+    bool PassiveTCPClient::StartWork(TCPClientSignal* pTCPClientSignal)
+    {
+        if (_bev)
+        {
             return false;
         }
         
         _bev = bufferevent_socket_new(NetFrame::_base,
                                       _fd,
                                       BEV_OPT_CLOSE_ON_FREE|BEV_OPT_THREADSAFE);
-        if (_bev == nullptr) {
+        if (_bev == nullptr)
+        {
             return false;
         }
         
@@ -98,7 +102,8 @@ namespace NAME_SPACE {
                            _fd,
                            EV_TIMEOUT|EV_PERSIST,
                            PassiveTCPTimeOutEventCb, this);
-        if (_event == nullptr) {
+        if (_event == nullptr)
+        {
             bufferevent_free(_bev);
             _bev = nullptr;
             return false;
@@ -118,50 +123,58 @@ namespace NAME_SPACE {
         return true;
     }
     
-    void PassiveTCPClient::StopWork() {
-        
-        if (_bev) {
+    void PassiveTCPClient::StopWork()
+    {
+        if (_bev)
+        {
+            LOG(INFO)<<"连接关闭:"<<_client_ip<<":"<<_client_port;
             bufferevent_disable(_bev, EV_READ);
             bufferevent_free(_bev);
             _bev = nullptr;
         }
         
-        if (_event) {
+        if (_event)
+        {
             event_del(_event);
             event_free(_event);
             _event = nullptr;
         }
-
-        LOG(INFO)<<"连接关闭:"<<_client_ip<<":"<<_client_port;
         
         // 不要对_pPassiveTCPClientSignal置null，释放由外部传入者负责
     }
     
-    int PassiveTCPClient::SendData(const sSendDataPage_ptr& pData) {
-        
-        if (nullptr == _bev || nullptr == pData) {
+    int PassiveTCPClient::SendData(const sSendDataPage_ptr& pData)
+    {
+        if (nullptr == _bev || nullptr == pData)
+        {
             return FUNC_FAILED;
         }
 
-        if (bufferevent_write(_bev, pData->send_buf, pData->send_len) < 0) {
+        if (bufferevent_write(_bev, pData->send_buf, pData->send_len) < 0)
+        {
             return FUNC_FAILED;
         }
         
         return FUNC_SUCCESS;
     }
 
-    void PassiveTCPClient::ProcEvent(EM_NET_EVENT event) {
-        
-        if (!_pTCPClientSignal) {
+    void PassiveTCPClient::ProcEvent(EM_NET_EVENT event)
+    {
+        if (!_pTCPClientSignal)
+        {
             return;
         }
         
-        if (ENE_HEART == event) {
+        if (ENE_HEART == event)
+        {
 
             // 如果超过三次，说明客户端已经死了，这个时候需要关闭
-            if(++_heart_num > 2) {
+            if(++_heart_num > 2)
+            {
                 event = ENE_HEART_TIMEOUT;
-            } else {
+            }
+            else
+            {
                 return;
             }
 
@@ -171,12 +184,13 @@ namespace NAME_SPACE {
         
     }
 
-    void PassiveTCPClient::PutRecvData(const unsigned char* buf, PacketLength len) {
-
+    void PassiveTCPClient::PutRecvData(const unsigned char* buf, PacketLength len)
+    {
         // 有数据往来,心跳计数清零
         _heart_num = 0;
 
-        if (nullptr == _pTCPClientSignal) {
+        if (nullptr == _pTCPClientSignal)
+        {
             return;
         }
 
