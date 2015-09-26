28b56db37714c9b9815ef50c2dafe3f1b4bcf703
cjj <chenjianjun571@gmail.com>
2015-09-26 16:45:11 +0800

    weqweqw

--- bss/src/communicate/communicate_protoc/ProtocolProcManager.cpp
+++ bss/src/communicate/communicate_protoc/ProtocolProcManager.cpp
@@ -14,6 +14,17 @@
 
 using namespace jsbn::protoc;
 
+void delete_recv_page(TProtocolBase* p)
+{
+    switch(p->command_id) {
+    case jsbn::protoc::MSG::Login_Request:
+    {
+        CObjectAllocator<TLogin>::get_instance()->free((TLogin*)p);
+        break;
+    }
+    }
+}
+
 sNetProtocolDataPage_ptr ProtocolProcManager::ParseProtocol(const unsigned char* buf, PacketLength len)
 {
     // 解析协议，生成一个协议的智能指针区域
@@ -26,7 +37,26 @@ sNetProtocolDataPage_ptr ProtocolProcManager::ParseProtocol(const unsigned char*
         return sNetProtocolDataPage_ptr();
     }
 
-    GetRecvData(protocol->type());
+
+    std::shared_ptr<TProtocolBase> ptr = GetRecvData(protocol->type());
+    if(protocol->type() == Login_Request)  {
+        ptr->command_id = protocol->type();
+        ::memcpy(((TLogin*)ptr.get())->user_name, protocol->loginrequest().username().c_str(), sizeof(((TLogin*)ptr.get())->user_name)-1);
+        ::memcpy(((TLogin*)ptr.get())->pass_word, protocol->loginrequest().password().c_str(), sizeof(((TLogin*)ptr.get())->pass_word)-1);
+        LOG(ERROR)<<"收到登录应答,用户名:"<<((TLogin*)ptr.get())->user_name<<":密码"<<((TLogin*)ptr.get())->pass_word;
+    }
 
     return protocol;
+}
+
+std::shared_ptr<TProtocolBase> ProtocolProcManager::GetRecvData(jsbn::protoc::MSG type) {
+
+    switch(type) {
+    case jsbn::protoc::MSG::Login_Request:
+    {
+        return std::shared_ptr<TProtocolBase>(CObjectAllocator<TLogin>::get_instance()->malloc(), delete_recv_page);
+        break;
+    }
+    }
+
 }
