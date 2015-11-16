c44d5c30112edda71914ddbc9296225d04c5bbd2
cjj <chenjianjun571@gmail.com>
2015-11-16 10:16:12 +0800

    21312312

--- css/src/communicate/rpc/RpcServiceHandler.cpp
+++ css/src/communicate/rpc/RpcServiceHandler.cpp
@@ -24,13 +24,18 @@ bool RpcServiceHandler::postUserInfo(const UserInfo& userInfo)
 void RpcServiceHandler::searchUserInfos(UserInfoSearchResult& _return, const std::string& query)
 {
     UserInfoList userInfos;
-    UserInfo userInfo;
-    userInfo.userId = 123456;
-    userInfo.userName = "我是服务器";
-    userInfo.language = "chian";
-    userInfo.cityID = CityID::type::CID_CQ;
-    userInfo.text = "阿海珐哈佛卡拉回访老客户分类客户发货";
-    userInfos.push_back(userInfo);
+
+    for(int i = 0; i < 3000; ++i)
+    {
+        UserInfo userInfo;
+        userInfo.userId = 123456;
+        userInfo.userName = "我是服务器";
+        userInfo.language = "chian";
+        userInfo.cityID = CityID::type::CID_CQ;
+        userInfo.text = "阿海珐哈佛卡拉回访老客户分类客户发货";
+        userInfos.push_back(userInfo);
+    }
+
     _return.__set_userinfos(userInfos);
 
     LOG(INFO)<<"searchUserInfos被调用"<<query;
